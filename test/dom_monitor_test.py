27ba4f5e1d4bf01dc081434cc7229d49950933a5
Koichi Taura <taura.koichi@gmail.com>
2019-07-23 22:57:54 +0900

    add red pattern of dom monitor of get database

--- test/dom_monitor_test.py
+++ test/dom_monitor_test.py
@@ -7,3 +7,10 @@ class DomMonitorTest(TestCase):
         client = DomMonitor.get_client("test")
         self.assertTrue(callable(client.get_html))
 
+    def test_get_database(self):
+        db = DomMonitor.get_database("test", {"hostname": "localhost", "port": "3301", "database": "dom_monitor"})
+        self.assertTrue(callable(db.get_exec_count))
+        self.assertTrue(callable(db.get_previous_html))
+        self.assertTrue(callable(db.update_exec_count))
+        self.assertTrue(callable(db.update_previous_html))
+
