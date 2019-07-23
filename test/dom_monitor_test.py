ed04721d361e211d90822b8ed382c9a72a3c1f7f
Koichi Taura <taura.koichi@gmail.com>
2019-07-23 22:43:50 +0900

    add red pattern of get client

--- test/dom_monitor_test.py
+++ test/dom_monitor_test.py
@@ -1,6 +1,9 @@
+from dom_monitor import DomMonitor
 from unittest import TestCase
 
 
 class DomMonitorTest(TestCase):
-    def test_failure(self):
-        self.fail("失敗")
+    def test_get_client(self):
+        client = DomMonitor.get_client("test")
+        self.assertTrue(callable(client.get_html))
+
