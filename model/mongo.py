505089d701d025a35c6f19a184a2ba3621254165
Koichi Taura <taura.koichi@gmail.com>
2019-08-05 20:46:53 +0900

    add app config and read from model

--- model/mongo.py
+++ model/mongo.py
@@ -8,8 +8,8 @@ class Mongo(Database):
         self.setting = setting
         username = urllib.parse.quote_plus(setting['username'])
         password = urllib.parse.quote_plus(setting['password'])
-        self.client = MongoClient('mongodb://%s:%s@localhost:27017/' % (username, password))
-        self.db = self.client['monitor']
+        self.client = MongoClient('mongodb://%s:%s@%s:%s/' % (username, password, setting['hostname'], setting['port']))
+        self.col = self.client[setting['database']][setting['collection']]
 
     def __del__(self):
         self.client.close()
