07fd503c30c385879fcf727b234815df7ade8d5c
Koichi Taura <taura.koichi@gmail.com>
2019-08-05 20:00:16 +0900

    update mongo connecting constructor

--- model/mongo.py
+++ model/mongo.py
@@ -1,9 +1,18 @@
 from model.db import Database
+from pymongo import MongoClient
+import urllib.parse
 
 
 class Mongo(Database):
     def __init__(self, setting):
         self.setting = setting
+        username = urllib.parse.quote_plus(setting['username'])
+        password = urllib.parse.quote_plus(setting['password'])
+        self.client = MongoClient('mongodb://%s:%s@localhost:27017/' % (username, password))
+        self.db = self.client['monitor']
+
+    def __del__(self):
+        self.client.close()
 
     def get_exec_count(self):
         pass
