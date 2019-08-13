3d62a8fde99c1990884b98dd15203411145a28f3
Taurin190 <taura.koichi@gmail.com>
2019-08-13 22:42:48 +0900

    add abstract feature

--- domain/monitor_logic.py
+++ domain/monitor_logic.py
@@ -1,8 +1,15 @@
+from domain.dom_diff import DomDiff
+
 class MonitorLogic:
-    def __init__(self, client, database, notification):
+    def __init__(self, client, database, notification, config):
         self.client = client
         self.database = database
         self.notification = notification
+        self.config = config
 
     def exec(self):
-        pass
+        exec_count = self.database.get_exec_count()
+        prev_html = self.database.get_previous_html()
+        current_html = self.client.get_html(self.config["url"])
+        diff_tool = DomDiff(self.config)
+        results = diff_tool.compare(prev_html, current_html)
